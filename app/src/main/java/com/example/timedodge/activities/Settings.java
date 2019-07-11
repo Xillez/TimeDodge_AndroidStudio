78c5c3d67f87919b1df9ef63dcd2033b460998b6
Xillez <kent.wincent46@gmail.com>
2019-07-11 15:44:37 +0200

    Optimized the settings activity, pluss added other disabled features for future sensitivity- and music setting

--- app/src/main/java/com/example/timedodge/activities/Settings.java
+++ app/src/main/java/com/example/timedodge/activities/Settings.java
@@ -19,6 +19,8 @@ public class Settings extends AppCompatActivity
 
     private AudioManager audioMgr = null;
 
+    private int tempLevel = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -28,33 +30,44 @@ public class Settings extends AppCompatActivity
         // Add toolbar and remove the title
         toolbarMgr.makeToolBar(this, R.id.toolbar02_back, true, this::finish);
 
+        // Get shared preferences
         this.sharedPref = this.getSharedPreferences(getString(R.string.sharedpref_name), Context.MODE_PRIVATE);
         this.editor = sharedPref.edit();
 
+        // Find the audiomanager
         this.audioMgr = ( AudioManager ) getSystemService(Context.AUDIO_SERVICE);
 
+        //Find soundbar
         SeekBar soundBar = findViewById(R.id.settings_seekbar_soundlevel);
         soundBar.setMax(15);
+
         if (audioMgr != null)
-            soundBar.setProgress(audioMgr.getStreamVolume(AudioManager.STREAM_MUSIC));
+            this.tempLevel = audioMgr.getStreamVolume(AudioManager.STREAM_MUSIC);
         else
-            soundBar.setProgress(sharedPref.getInt(getString(R.string.sharedpref_sound_key), 0));
+            this.tempLevel = sharedPref.getInt(getString(R.string.sharedpref_sound_key), 0);
+
+        soundBar.setProgress(this.tempLevel);
+
         soundBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b)
             {
-                editor.putInt(getString(R.string.sharedpref_sound_key), i);
-                editor.apply();
-                if (audioMgr != null)
-                    audioMgr.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+                tempLevel = i;
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+                editor.putInt(getString(R.string.sharedpref_sound_key), tempLevel);
+                editor.apply();
+                if (audioMgr != null)
+                    audioMgr.setStreamVolume(AudioManager.STREAM_MUSIC, tempLevel, 0);
+            }
         });
+
         /*SeekBar soundBar = findViewById(R.id.settings_seekbar_musiclevel);
         soundBar.setProgress(sharedPref.getInt(getString(R.string.sharedpref_music_key), 0));
         soundBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
@@ -62,6 +75,24 @@ public class Settings extends AppCompatActivity
             public void onProgressChanged(SeekBar seekBar, int i, boolean b)
             {
                 editor.putInt(getString(R.string.sharedpref_music_key), i);
+                editor.apply();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });*/
+
+        /*SeekBar tiltSensBar = findViewById(R.id.settings_seekbar_sensorsensitivity);
+        soundBar.setMax(100);
+        tiltSensBar.setProgress(sharedPref.getInt(getString(R.string.settings_text_sensorsensitivity), 0));
+        tiltSensBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+            {
+                editor.putInt(getString(R.string.sharedpref_sensitivity_key), i);
                 editor.apply();
             }
 
