e2f1668c1d952091fc7cf520744a583d21f93150
Xillez <kent.wincent46@gmail.com>
2019-07-11 15:48:14 +0200

    Additional changes to renaming of GameCanvas to GameManager

--- app/src/main/java/com/example/timedodge/game/GameMode.java
+++ app/src/main/java/com/example/timedodge/game/GameMode.java
@@ -1,13 +1,10 @@
 package com.example.timedodge.game;
 
 import android.content.Context;
-import android.content.res.XmlResourceParser;
-
-import com.example.timedodge.R;
 
 public class GameMode
 {
-    public static void loadGamemode(Context context, GameCanvas gameCanvas)
+    public static void loadGamemode(Context context, GameManager gameManager)
     {
         //XmlResourceParser parser = context.getResources().getXml(R.raw.gamemode_default);
     }
--- app/src/main/java/com/example/timedodge/game/Public.java
+++ app/src/main/java/com/example/timedodge/game/Public.java
@@ -7,6 +7,7 @@ import com.example.timedodge.game.event.GameEventHandler;
 public class Public
 {
     public static PointF screenSize = new PointF(0, 0);
+
     public static GameEventHandler gameEventHandler = new GameEventHandler();
-    public static GameCanvas canvas = null;
+    public static GameManager canvas = null;
 }
